da670584949d9fdb3f806dd7029da555ecb99d7b
abunusaybah <your_github_email@example.com>
2025-11-14 10:24:27 +0100

    Updated file structures

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -6,8 +6,7 @@ const Home: React.FC = () => {
 			<Header />
 			<main className="flex-grow flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
 				<div className="text-center">
-					<h1 className="text-5xl font-bold text-white">
-						Welcome to our Application!
+					<h1 className="text-5xl font-bold text-white"> Welcome to our Application!
 					</h1>
 					<p className="mt-4 text-xl text-white">
 						We&apos;re glad you&apos;re here. Explore and enjoy your experience.
