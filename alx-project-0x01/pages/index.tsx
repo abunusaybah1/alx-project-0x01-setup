58ba84d78a184829cec58fcbf3ba1f98730171e6
abunusaybah <your_github_email@example.com>
2025-11-15 12:45:26 +0100

    update

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -4,9 +4,11 @@ const Home: React.FC = () => {
 	return (
 		<div className="flex flex-col h-screen">
 			<Header />
-			<main className="flex-grow flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+			<main className="flex grow items-center justify-center bg-linear-to-r from-blue-500 to-purple-600">
 				<div className="text-center">
-					<h1 className="text-5xl font-bold text-white"> Welcome to our Application!
+					<h1 className="text-5xl font-bold text-white">
+						{" "}
+						Welcome to our Application!
 					</h1>
 					<p className="mt-4 text-xl text-white">
 						We&apos;re glad you&apos;re here. Explore and enjoy your experience.
